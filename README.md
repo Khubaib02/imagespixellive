b37d4b63f3abb44cd7957b9b7a89a7440534d027
Khubaib02 <khubaibafaq18@gmail.com>
2024-06-14 16:21:48 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# imagespixellive
+# colorz-picker
+https://ziadevcom.github.io/colorz-picker/
+</br>
+## A Minimal Color Picker</br>
+<img src='./screenshot.jpg'/>
---
+++ url.js
@@ -0,0 +1,9 @@
+
+
+export const url_light = 'https://api.imagespixel.com:8000';
+
+export const url_heavy = 'https://api-heavy.imagespixel.com:8005';
+
+// export const url_light = 'http://192.168.10.33:8001';
+
+// export const url_heavy = 'http://192.168.10.33:8001';
